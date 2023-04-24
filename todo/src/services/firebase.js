e8357b9f90748a3a8568aa05f1a9bb6427e87abc
Avin-saxena <avin12121111@gmail.com>
2023-04-25 00:33:05 +0530

    commit

--- todo/src/services/firebase.js
+++ todo/src/services/firebase.js
@@ -1,19 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { firebaseConfig } from "./firebaseConfig";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBO-oD4Em6HeN3o2YzRS_NHSaHX-QuNeB8",
+  authDomain: "todo-46e8a.firebaseapp.com",
+  projectId: "todo-46e8a",
+  storageBucket: "todo-46e8a.appspot.com",
+  messagingSenderId: "10413506652",
+  appId: "1:10413506652:web:3d0d7da87b2711fea7e699",
+  measurementId: "G-H9BWENM8SF"
 };
 
-initializeApp(firebaseConfig);
-
-const auth = getAuth();
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export default {
   auth
 };
